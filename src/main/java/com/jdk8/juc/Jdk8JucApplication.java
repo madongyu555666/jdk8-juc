8d06e3b89fd415e9628e1d520015971ed5ce2021
madongyu555666 <18333510375@163.com>
2020-06-11 21:33:52 +0800

    Initial commit

---
+++ src/main/java/com/jdk8/juc/Jdk8JucApplication.java
@@ -0,0 +1,13 @@
+package com.jdk8.juc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jdk8JucApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Jdk8JucApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jdk8/juc/Jdk8JucApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jdk8.juc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jdk8JucApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
